eba3c8d8e77cc7846027cc1d888c5acc6739a6d1
Kiran Davuluri <75129341+kiran-collab@users.noreply.github.com>
2021-03-23 16:41:51 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
-var browser = require("webextension-polyfill");
- 
+
 browser.runtime.onMessage.addListener(async (msg, sender) => {
   console.log("BG page received message", msg, "from", sender);
   console.log("Stored data", await browser.storage.local.get());
